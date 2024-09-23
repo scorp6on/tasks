dc22b32000dc036ab413b1c8c1262b06927e4f06
Ronald <ronald@udel.edu>
2024-09-23 11:34:35 -0400

    weird

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import { Button } from "react-bootstrap";
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
->>>>>>> upstream/task-state
 
 function App(): React.JSX.Element {
     return (
@@ -27,62 +23,6 @@ function App(): React.JSX.Element {
                     Hello World
                 </h1>
             </header>
-<<<<<<< HEAD
-
-            <img
-                src="C:\Users\vgnna\OneDrive\Pictures\yu gi oh.PNG"
-                alt="YuGiOh"
-            />
-
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-
-            <ul>
-                <li>First element</li>
-                <li>Second element</li>
-                <li>Third element</li>
-            </ul>
-
-            <Button
-                onClick={() => {
-                    console.log("Hello World!");
-                }}
-            >
-                Log Hello World
-            </Button>
-
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "space-around",
-                    marginTop: "20px",
-                }}
-            >
-                <div
-                    style={{
-                        width: "100px",
-                        height: "100px",
-                        backgroundColor: "red",
-                    }}
-                ></div>
-                <div
-                    style={{
-                        width: "100px",
-                        height: "100px",
-                        backgroundColor: "red",
-                    }}
-                ></div>
-                <div
-                    style={{
-                        width: "100px",
-                        height: "100px",
-                        backgroundColor: "red",
-                    }}
-                ></div>
-            </div>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -95,7 +35,6 @@ function App(): React.JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
